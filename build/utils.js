1ec9238bc1e8ec9afe8b7065914d259de5a975f9
kirill <7769424+pretzelhammer@users.noreply.github.com>
2022-05-25 12:49:04 -0400

    adds some filled icons locally (#10)

--- build/utils.js
+++ build/utils.js
@@ -3,17 +3,20 @@ const path = require('path');
 const { camelCase, upperFirst } = require('lodash');
 const pascal = string => upperFirst(camelCase(string));
 
-const featherIconsDir = path.dirname(require.resolve('feather-icons/package.json'));
-const featherIconsAssetsDir = path.resolve(featherIconsDir, 'dist/icons/');
-
 function getIconEntries(dir) {
-	const iconPaths = fs.readdirSync(featherIconsAssetsDir)
+	const iconPaths = fs.readdirSync(dir)
 		.filter(f => f.endsWith('.svg'));
 
 	const entries = {};
 	iconPaths.forEach(file => {
-		entries[path.join(dir, pascal(path.basename(file, '.svg')))] = path.join('feather-icons/dist/icons/', file);
+		const isRelative = dir.startsWith('./');
+		let svgPath = path.join(dir, file);
+		if (isRelative) {
+			svgPath = `./${svgPath}`;
+		}
+		entries[pascal(path.basename(file, '.svg'))] = svgPath;
 	});
+
 	return entries;
 }
 
